e411cf558cbcf69efd3f5730701c8fa6e0a5c2fc
Shahzod <shahzodtashev49@gmail.com>
2023-08-13 17:03:30 +0500

    .

--- src/api/weather.js
+++ src/api/weather.js
@@ -3,11 +3,11 @@ import axios from "axios"
 const API_KEY = '94caff0a566f42cfa7263537230608'
 
 export const getCurrentWeather = async (lat, long) => {
-        const response = await axios.get(`http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${lat},${long}&aqi=no`);
+        const response = await axios.get(`https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${lat},${long}&aqi=no`);
         return response.data
 }
 
 export const getWeeklyWeather = async (lat, long) => {
-        const response = await axios.get(`http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${lat},${long}&days=7&aqi=no&alerts=no`);
+        const response = await axios.get(`https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${lat},${long}&days=7&aqi=no&alerts=no`);
         return response.data
 }
